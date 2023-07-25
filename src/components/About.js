458a0271830f7ffbb9a2305ce9dfecbddd67be8a
Khalid Zein <zeinkhalid004@gmail.com>
2023-07-25 12:46:10 +0300

    home page responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 function About() {
     return(
+        <div className="about-parent">
         <div className="about">
             {/* <p className="one">Welcome to ....................</p> */}
             <p className="two">مكتب تسجيل أنساب السادة آل باعلوي
 شرق إفريقيا بدار المدثر - ماليندي كينيا</p>
             <p>Ofisi ya Usajili wa Nasaba ya Al-Baalawi Mashariki ya Afrika. Dar Al-Muddathir - Malindi, Kenya</p>
             <p>Genealogy Registration Office of Al-Baalawi East Africa. Dar Al-Muddathir - Malindi, Kenya</p>
+        </div>
         </div>
     )
 }
