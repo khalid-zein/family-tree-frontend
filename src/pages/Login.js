893440b0f90ab9f1f20c22a44faafc08a5a2a00e
Khalid Zein <zeinkhalid004@gmail.com>
2023-07-27 23:14:11 +0300

    login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,29 @@
 function Login() {
   return (
     <div className="login">
-      <p>Login Page</p>
+      <div className="login-container">
+
+        <div className="login-one">
+          <div>
+            <h3><i class="fa-solid fa-triangle-exclamation"></i></h3>
+            <p className="p-one">Limited access</p>
+          </div>
+        </div>
+
+        <div className="login-two">
+          <div className="login-child">
+            <p>Sign in</p>
+            <form>
+              <div>
+                 <input className="entry" type="email" placeholder="Email"></input>
+                 <input className="entry" type="password" placeholder="password"></input>
+              </div>
+              <button>Submit</button><br></br>
+              <a href="#">forgot password ?</a>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
