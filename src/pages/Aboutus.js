6251504cfa14485313e7ccfe06aa89fdada0d603
Khalid Zein <zeinkhalid004@gmail.com>
2023-08-02 16:11:16 +0300

    about page

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -9,14 +9,150 @@ function Aboutus() {
 
   return (
     <div className="history">
-      <select onChange={(e) => handleClick(e.target.value)}>
-        <option>Choose language</option>
-        <option value="ar">عربي</option>
-        <option value="en">English</option>
-      </select>
+      <div className="upper-bg"></div>
+      <div className="lower-bg">
+        <div className="content-bg">
+          <select onChange={(e) => handleClick(e.target.value)}>
+            <option>Choose language</option>
+            <option value="ar">عربي</option>
+            <option value="en">English</option>
+          </select>
+          <p className="p-one">{t("1.1")}</p>
 
-      <div className="aboutus">
-        <p>{t("About.1")}</p>
+          <div>
+            <h2>{t("2.1")}</h2>
+            <p>{t("3.1")}</p>
+          </div>
+
+          <div>
+            <h2>{t("4.1")}</h2>
+            <p>{t("5.1")}</p>
+          </div>
+
+          <div>
+            <h2>{t("6.1")}</h2>
+            <p>{t("7.1")}</p>
+          </div>
+
+          <div>
+            <h2>{t("8.1")}</h2>
+            <p>{t("9.1")}</p>
+          </div>
+
+          <div>
+            <h2>{t("10.1")}</h2>
+            <p>{t("11.1")}</p>
+          </div>
+
+          <div>
+            <h2>{t("12.1")}</h2>
+            <p>{t("13.1")}</p>
+          </div>
+
+          <div>
+            <h2>{t("14.1")}</h2>
+            <p>{t("15.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("16.1")}</p>
+            <p>{t("17.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("18.1")}</p>
+            <p>{t("19.1")}</p>
+            <p>{t("20.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("21.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("22.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("23.1")}</p>
+            <p>{t("24.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("25.1")}</p>
+            <p>{t("26.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("27.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("28.1")}</p>
+            <p>{t("29.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("30.1")}</p>
+            <p>{t("31.1")}</p>
+            <p>{t("32.1")}</p>
+            <p>{t("33.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("34.1")}</p>
+            <p>{t("35.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("36.1")}</p>
+            <p>{t("37.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("38.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("39.1")}</p>
+            <p>{t("40.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("41.1")}</p>
+            <p>{t("42.1")}</p>
+            <p>{t("43.1")}</p>
+          </div>
+          <div>
+            <p>{t("44.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("45.1")}</p>
+            <p>{t("46.1")}</p>
+          </div>
+
+          <div>
+            <p>{t("47.1")}</p>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
