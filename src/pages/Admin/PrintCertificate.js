0612abde2e7b92fa18976422800516ece9bab414
root <andrewomosh44@gmail.com>
2024-01-18 10:26:37 +0300

    add certificate to print custom template

--- src/pages/Admin/PrintCertificate.js
+++ src/pages/Admin/PrintCertificate.js
@@ -1,66 +1,77 @@
-import React, { useState} from 'react';
-import { BsPencilSquare } from "react-icons/bs"
-import { BiTrash } from "react-icons/bi"
-import { dataUrl } from "../../data/ApiUrls";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import ReactDOMServer from 'react-dom/server';
-import certImage from '../../assets/img/baalawi-certificate.png';
-import "../../index.css"
+import React, { useState } from 'react';
+import { BsPencilSquare } from 'react-icons/bs';
+import { BiTrash } from 'react-icons/bi';
+import { dataUrl } from '../../data/ApiUrls';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { PDFDocument, StandardFonts } from 'pdf-lib';
+import jsPDF from 'jspdf';
+import '../../index.css';
+
+import customPdfTemplate from '../../assets/cert.pdf';
 
 const PrintCertificate = ({ members, setData, loading, error }) => {
   const [selectedMember, setSelectedMember] = useState(null);
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
 
   const handleDeleteMember = async (id) => {
-    axios.delete(`${dataUrl}/update-delete/${id}`)
-        .then((res) => {
-            const newMembers = members.filter((member) => member.id !== id)
-            setData(newMembers)
-            window.location.reload()
-        })
-        .catch(err => console.error(err));
-  }
+    try {
+      await axios.delete(`${dataUrl}/update-delete/${id}`);
+      const newMembers = members.filter((member) => member.id !== id);
+      setData(newMembers);
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const handlePrintDetails = (member) => {
     setSelectedMember(member);
     printCertificate();
   };
 
-  const printCertificate = () => {
+  const printCertificate = async () => {
+    const existingPdfBytes = await fetch(customPdfTemplate).then((res) => res.arrayBuffer());
+
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    const page = pdfDoc.getPage(0);
+
+    // Add user details to the PDF
+    // const font = await pdfDoc.embedFont(PDFDocument.Font.Helvetica)
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);  
+    // const font = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+
+    page.drawText(`This Certificate is to certify that ${selectedMember?.user_name}`, {
+      x: 100,
+      y: 550,
+      font: helveticaFont,
+      fontWeight: 800,
+      size: 12,
+    });
+
+    page.drawText('is a member of the Baalawi Family & the foloowing are the grandparents:', {
+      x: 100,
+      y: 530,
+      font: helveticaFont,
+      size: 12,
+    });
+
+    if (selectedMember?.parent.length > 0) {
+      const parentDetails = selectedMember.parent.map((parent) => `${parent.id}: ${parent.user_name}`).join('\n');
+      page.drawText(parentDetails, { x: 100, y: 510, font: helveticaFont, size: 12 });
+    }
+
+    const modifiedPdfBytes = await pdfDoc.save();
+    const modifiedPdfBlob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
+
     const printWindow = window.open('', '_blank');
-    const printContent = (
-      <div className="print-content">
-        <img src={certImage} alt="Certificate Image" />
-        <div className="certificate-text">
-          <h2>This Certificate is to certify that {selectedMember?.user_name}</h2>
-          <h4>is a member of the Baalawi Family & is related to:</h4>
-            {selectedMember?.parent.length > 0 && (
-              <div>
-                {selectedMember.parent.map((parent, parentIndex) => (
-                    <p key={parentIndex}>{parent.id}: {parent.user_name}</p>
-                ))}
-              </div>
-            )}
-        </div>
-      </div>
-    );
-
-    // Render the React component to an HTML string
-    const htmlString = ReactDOMServer.renderToStaticMarkup(printContent);
-
-    // Set the HTML string in the print window
-    printWindow.document.body.innerHTML = htmlString;
-
-    // Trigger the print dialog
-    printWindow.print();
-
-    // Close the new window after printing
-    printWindow.close();
+    const printUrl = URL.createObjectURL(modifiedPdfBlob);
+    printWindow.location.href = printUrl;
   };
 
-    const filteredMembers = members.filter((member) => member.user_name.toLowerCase().includes(input.toLowerCase()))
-    // console.log(filteredMembers)
+  const filteredMembers = members.filter((member) =>
+    member.user_name.toLowerCase().includes(input.toLowerCase())
+  );
 
   return (
     <>
@@ -68,7 +79,7 @@ const PrintCertificate = ({ members, setData, loading, error }) => {
         <input
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          placeholder='Search member..' 
+          placeholder="Search member.." 
         />
       </form>
 
@@ -76,55 +87,54 @@ const PrintCertificate = ({ members, setData, loading, error }) => {
         <p>{error}</p>
         {loading ? (
           <p>Loading member's list..</p>
-        ): (
+        ) : (
           <div>
             <p>{error}</p>
-            <table class="table-auto">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Parent's Name</th>
-                        <th>Child's Name</th>
-                        <th>Update</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
+            <table className="table-auto">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Parent's Name</th>
+                  <th>Child's Name</th>
+                  <th>Update</th>
+                  <th>Delete</th>
+                  <th>Print Certificate</th>
+                </tr>
+              </thead>
+              <tbody>
                 {filteredMembers.map((member, index) => (
-                    <tbody key={index}>
-                        <tr>
-                            <td>{member.id}</td>
-                            <td>
-                                {member.user_name}
-                            </td>
-                                <td>
-                                {member.parent.length > 0 && (
-                                  <div>
-                                    {member.parent.map((parent, parentIndex) => (
-                                      <p key={parentIndex}>{parent.user_name}</p>
-                                    ))}
-                                  </div>
-                                )}  
-                                </td>
-                            <td className="">
-                                <Link to={`/admin/edit-member/${member.id}/`}>
-                                    <button>
-                                        <BsPencilSquare />
-                                    </button>
-                                </Link>
-                            </td>
-                            <td>
-                                <button onClick={() => handleDeleteMember(member.id)}>
-                                    <BiTrash /> 
-                                </button>
-                            </td>
-                            <td>
-                                <button onClick={() => handlePrintDetails(member)}>
-                                    Print Certificate
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
+                  <tr key={index}>
+                    <td>{member.id}</td>
+                    <td>{member.user_name}</td>
+                    <td>
+                      {member.parent.length > 0 && (
+                        <div>
+                          {member.parent.map((parent, parentIndex) => (
+                            <p key={parentIndex}>{parent.user_name}</p>
+                          ))}
+                        </div>
+                      )}
+                    </td>
+                    <td className="">
+                      <Link to={`/admin/edit-member/${member.id}/`}>
+                        <button>
+                          <BsPencilSquare />
+                        </button>
+                      </Link>
+                    </td>
+                    <td>
+                      <button onClick={() => handleDeleteMember(member.id)}>
+                        <BiTrash /> 
+                      </button>
+                    </td>
+                    <td>
+                      <button onClick={() => handlePrintDetails(member)}>
+                        Print Certificate
+                      </button>
+                    </td>
+                  </tr>
                 ))}
+              </tbody>
             </table>
           </div>
         )}
